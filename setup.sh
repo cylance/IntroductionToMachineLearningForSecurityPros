a416d237e46c6c76978f5c894a3dfd3846aebdfe
bwall <bwall9809@gmail.com>
2017-08-21 16:31:07 -0700

    Update setup.sh some more

--- setup.sh
+++ setup.sh
@@ -2,8 +2,12 @@
 sudo apt-get update;
 sudo apt-get upgrade -y;
 
-sudo apt-get install python-dev python-pip git libhdf5-dev python-tk libfuzzy-dev libffi-dev -y
+sudo apt-get install python-dev python-pip git libhdf5-dev python-tk libfuzzy-dev libffi-dev graphviz -y
 
 sudo pip install numpy scipy sklearn keras theano tensorflow h5py matplotlib gevent requests ssdeep
 
 git clone https://github.com/CylanceSPEAR/IntroductionToMachineLearningForSecurityPros.git
+
+cd IntroductionToMachineLearningForSecurityPros
+
+find . -type f -name \*.lzma -exec lzma -d {} \;
