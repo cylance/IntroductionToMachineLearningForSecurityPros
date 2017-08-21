8a7442705f1ff7f21a651e621faf4fe41a61f050
bwall <bwall9809@gmail.com>
2017-08-21 15:43:58 -0700

    Updated setup.sh and removed a bunch of error logs

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 sudo apt-get update;
 sudo apt-get upgrade -y;
 
-sudo apt-get install python-dev python-pip git libhdf5-dev python-tk -y
+sudo apt-get install python-dev python-pip git libhdf5-dev python-tk libfuzzy-dev libffi-dev -y
 
-sudo pip install numpy scipy sklearn keras theano tensorflow h5py matplotlib
+sudo pip install numpy scipy sklearn keras theano tensorflow h5py matplotlib gevent requests ssdeep
 
 git clone https://github.com/CylanceSPEAR/IntroductionToMachineLearningForSecurityPros.git
